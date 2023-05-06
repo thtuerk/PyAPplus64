b05b5de039d827dd266801184c07d55fc7f29a7b
Thomas Türk <kontakt@thomas-tuerk.de>
2023-05-06 22:19:51 +0200

    add Changelog and Version info to documentation

---
+++ Changelog.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 06.05.2023 v1.0.1
+- Code-Cleanup mit Hilfe von flake8
+- Bugfix: neue Python 3.10 Syntax entfernt
+- kleinere Verbesserungen an Doku
+
+## 04.05.2023 v1.0.0
+erste veröffentlichte Version
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ sys.path.append('../src/')
 project = 'PyAPplus64'
 copyright = '2023, Thomas Tuerk'
 author = 'Thomas Tuerk'
+version = '1.0.1'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
